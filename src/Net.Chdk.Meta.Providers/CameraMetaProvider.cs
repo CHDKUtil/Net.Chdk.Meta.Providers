87400347887c53142a9b4974e2176a61f2ccdb8a
Dmitry Shechtman <damnian@gmail.com>
2017-07-16 06:12:52 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers/CameraMetaProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net.Chdk.Meta.Providers
+{
+    sealed class CameraMetaProvider : ICameraMetaProvider
+    {
+        private IEnumerable<IProductCameraMetaProvider> CameraProviders { get; }
+
+        public CameraMetaProvider(IEnumerable<IProductCameraMetaProvider> cameraProviders)
+        {
+            CameraProviders = cameraProviders;
+        }
+
+        public CameraInfo GetCamera(string productName, string fileName)
+        {
+            var provider = CameraProviders.SingleOrDefault(p => productName.Equals(p.ProductName, StringComparison.Ordinal));
+            if (provider == null)
+                throw new InvalidOperationException($"Unknown product: {productName}");
+            return provider.GetCamera(fileName);
+        }
+    }
+}
